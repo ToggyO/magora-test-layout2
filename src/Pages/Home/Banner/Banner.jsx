97b207312edc35c2f1ca407f0c27d1a83061fa6e
unknown <Togushakov@magora.loc>
2019-07-10 11:10:18 +0700

    header+form

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,15 +5,17 @@ import './Banner.sass';
 const Banner = () => {
   return (
       <div className="banner wrapper">
-        <div className="banner-container wrapper-container">
-          <div className="banner-container-content">
+        <div className="banner-container wrapper-container d-f jc-c ai-c">
+          <div className="banner-container-content d-f fd-c">
             <div className="banner-headlines">
-              <h1 className="h2-white fs-75 ls-9 lh-82 fw-700">Pitch your idea</h1>
-              <h3 className="h2-white fs-40 ls-6 lh-55 fw-700">to improve your community</h3>
+              <h1 className="h2-white fs-75 ls-9 lh-82 fw-700 ml-35 mr-35">Pitch your idea</h1>
+              <h1 className="h2-white fs-40 ls-6 lh-55 fw-700 mb-10 ml-35 mr-35">to improve your community</h1>
             </div>
             <div className="banner-search-block">
-              <input className="banner-search sh-inputLarge input-lg" type="text" placeholder="Describe your idea"/>
-              <button className="btn-green-lg sh-btn-lg">Create project</button>
+              <form action="" className="search-block-form wrapper d-f">
+                <input className=" search-block-input sh-inputLarge input-lg ml-35 w-45" type="text" placeholder="Describe your idea"/>
+                <button className=" btn green md fs-18 ls-27 lh-18 fw-500 ml-20 sh-btn-lg mr-35 w-15">Create project</button>
+              </form>
             </div>
           </div>
         </div>
--- src/Pages/Home/Header/Logged/Logged.jsx
+++ src/Pages/Home/Header/Logged/Logged.jsx
@@ -4,10 +4,15 @@ import avatar from '../../../../img/avatar.jpg';
 
 const Header = () => {
   return (
-            <div className="header-changeGroup d-f">
-              <button className="btn-green-sm sh-btn-sm-op18 mr-12">Create Project</button>
-              <img className="sh-avatar" src={avatar} alt="user"/>
-            </div>
+    <div className="header-changeGroup d-f">
+      <button
+        style={{boxShadow: '0 8px 16px 0 rgba(0,0,0,0.18)'}}
+        className="btn green sm fs-18 lh-18 ls-27 fw-500 sh-btn-sm mr-12"
+      >
+        Create Project
+      </button>
+      <img className="sh-avatar" src={avatar} alt="user"/>
+    </div>
   );
 }
 
