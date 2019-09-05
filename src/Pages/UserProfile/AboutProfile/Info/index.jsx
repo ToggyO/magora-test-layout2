280dcdf97f64884be4e68145b5e2365dd1532e2b
Oleg <togushakov@magora-systems.com>
2019-09-05 14:37:12 +0700

    fixed profile link

--- src/Pages/UserProfile/AboutProfile/Info/index.jsx
+++ src/Pages/UserProfile/AboutProfile/Info/index.jsx
@@ -10,7 +10,7 @@ const Info = (props) => {
             <h6 className='h2-black fs-24 lh-43 ls-4 fw-600'>{title}</h6>
           </div>
           <div className='about-profile__link '>
-            <a href='#' className='h2-black fs-16 lh-24 ls-2 fw-500'>{value}</a>
+            <a href={value} className='h2-black fs-16 lh-24 ls-2 fw-500'>{value}</a>
           </div>
         </div>;
 };
